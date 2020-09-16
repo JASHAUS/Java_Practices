3c5003179cc0f5792b3da1a803dec466545b444c
JASHAUS <43691743+JASHAUS@users.noreply.github.com>
2020-09-16 22:31:15 +0530

    Day03_Java
    Day03_Java

---
+++ Day03/src/com/employee/Employee.java
@@ -0,0 +1,13 @@
+package com.employee;
+
+public class Employee {
+	public int id;
+	public String name;
+
+	public Employee(int i, String name) {
+		super();
+		this.id = i;
+		this.name = name;
+	}
+
+}
